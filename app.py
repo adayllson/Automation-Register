cce5985e0ccc105eea62cb1790cad12795e6b7a0
Adailson <adayllson.almeida@gmail.com>
2023-11-27 13:41:50 -0300

    configurando a segunda página

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import openpyxl
 import pyperclip
 import pyautogui
+from time import sleep
 
 # Entrar na planilha 
 workbook = openpyxl.load_workbook('produtos_ficticios.xlsx')
@@ -40,6 +41,45 @@ for linha in sheet_produtos.iter_rows(min_row=2):
     pyperclip.copy(dimensoes)
     pyautogui.click(1455,871, duration=1)
     pyautogui.hotkey('ctrl', 'v')
+
+    pyautogui.click(1441,943, duration=1)
+    sleep(3)
+
+    preco = linha[6].value
+    pyperclip.copy(preco)
+    pyautogui.click(1471,420, duration=1)
+    pyautogui.hotkey('ctrl', 'v')
+
+    quantidade_em_estoque = linha[7].value
+    pyperclip.copy(quantidade_em_estoque)
+    pyautogui.click(1539,503, duration=1)
+    pyautogui.hotkey('ctrl', 'v')
+
+    data_de_validade = linha[8].value
+    pyperclip.copy(data_de_validade)
+    pyautogui.click(1490,593, duration=1)
+    pyautogui.hotkey('ctrl', 'v')
+
+    cor = linha[9].value
+    pyperclip.copy(cor)
+    pyautogui.click(1474,670, duration=1)
+    pyautogui.hotkey('ctrl', 'v')
+
+    tamanho = linha[10].value
+    if tamanho == 'Pequeno':
+        pyautogui.click(1435,797, duration=1)
+        
+    elif tamanho == 'Médio':
+        pyautogui.click(1439,814, duration=1)
+        
+    else:
+        pyautogui.click(1437,837, duration=1)
+
+    material = linha[11].value
+    pyperclip.copy(material)
+    pyautogui.click(1431,855, duration=1)
+    pyautogui.hotkey('ctrl', 'v')
+        
 
     break
     # codigo_produto = linha[3].value
